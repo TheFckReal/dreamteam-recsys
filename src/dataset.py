67d672a1d7fefc7584ebb8f91e022e1048c3bfe4
The Real <sashasan2003@mail.ru>
2025-11-15 23:19:39 +0300

    add dataset load + date creation

--- src/dataset.py
+++ src/dataset.py
@@ -1,28 +1,77 @@
+import os
 from pathlib import Path
+from tempfile import TemporaryFile
+from typing import Annotated, Optional
 
 from loguru import logger
+import polars as pl
 from tqdm import tqdm
 import typer
 
-from src.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from src.config import RAW_DATA_DIR
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
+def download(
+    output_dir: Path = typer.Option(
+        RAW_DATA_DIR, help="Output directory for the dataset.", file_okay=False
+    ),
+    token: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Hugging Face token. If not provided,"
+            "will be taken from HF_TOKEN environment variable.",
+            envvar="HF_TOKEN",
+        ),
+    ] = None,
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+    from huggingface_hub import snapshot_download
+
+    if token is None:
+        token = os.getenv("HF_TOKEN")
+    if token is None:
+        raise ValueError("HF_TOKEN is not set")
+    logger.info("Downloading dataset to {output_dir}...", output_dir=output_dir)
+
+    snapshot_download(
+        repo_id="t-tech/T-ECD",
+        repo_type="dataset",
+        allow_patterns="dataset/small/",
+        local_dir=output_dir,
+        token=token,
+    )
+    logger.success("Dataset downloaded.")
+
+
+@app.command("add-marketplace-dates", help="Add marketplace events dates to the dataset")
+def add_marketplace_events_dates(
+    events_dir: Path = typer.Option(
+        RAW_DATA_DIR / "dataset" / "small" / "marketplace" / "events",
+        help="Path to the marketplace events directory.",
+        file_okay=False,
+        exists=True,
+    ),
+):
+    logger.info("Adding marketplace events dates...")
+    event_files = sorted(events_dir.glob("*.pq"))
+    if not event_files:
+        logger.exception("No event files found.")
+        raise FileNotFoundError(f"No event files found in {events_dir}")
+    for event_file in tqdm(event_files):
+        day_number = int(event_file.stem)
+        event_df = pl.scan_parquet(event_file)
+        event_df = event_df.with_columns(pl.lit(day_number, dtype=pl.Int32).alias("day"))
+        with TemporaryFile() as temp_file:
+            event_df.sink_parquet(temp_file)
+            temp_file.seek(0)
+            event_df = pl.scan_parquet(temp_file)
+            event_df.sink_parquet(event_file)
+    logger.success(
+        "Marketplace events dates added. Saved to {events_dir} with new column 'day'.",
+        events_dir=events_dir,
+    )
 
 
 if __name__ == "__main__":
