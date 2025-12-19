4328467fae8b6b5965c2d9274dba986e583d0084
AlexanderVictorov <alex.victorov.deals@yandex.ru>
2025-12-19 13:16:00 +0300

    Created history endpoint and service

--- src/service/backend/dependencies.py
+++ src/service/backend/dependencies.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 from src.service.backend.service import PredictionService
-
+from src.service.backend.service import PredictionService, HistoryService
 
 @lru_cache
 def get_prediction_service() -> PredictionService:
@@ -13,3 +13,8 @@ def get_prediction_service() -> PredictionService:
     # Использует @lru_cache для реализации Singleton (сервис создается один раз).
     service = PredictionService()
     return service
+
+#Провайдер зависимости для HistoryService
+@lru_cache
+def get_history_service() -> HistoryService:
+    return HistoryService()
