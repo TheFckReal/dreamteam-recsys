19561c69cb8305cd9aed059bda35ae3e0982d51a
The Real <102225891+TheFckReal@users.noreply.github.com>
2026-05-11 23:00:32 +0300

    Feature/model features (#26)
    * add features collecting and fixes dataset info
    
    * reformat
    
    * create vae model
    
    * add transformer (#24)
    
    Co-authored-by: Светлана Козина <sakozina@edu.hse.ru>
    
    * edit experiments_comparison.md (#25)
    
    * edit experiments_comparison.md
    
    * edit experiments_comparison.md 2
    
    * edit experiments_comparison.md 3
    
    ---------
    
    Co-authored-by: Светлана Козина <sakozina@edu.hse.ru>
    
    ---------
    
    Co-authored-by: Козина Светлана <60421735+lanary-k@users.noreply.github.com>
    Co-authored-by: Светлана Козина <sakozina@edu.hse.ru>

--- src/config.py
+++ src/config.py
@@ -20,6 +20,7 @@ EXTERNAL_DATA_DIR = DATA_DIR / "external"
 MODELS_DIR = PROJ_ROOT / "models"
 SVD_DIR = MODELS_DIR / "svd_artifacts"
 IALS_DIR = MODELS_DIR / "ials_artifacts"
+VAE_DIR = MODELS_DIR / "vae_artifacts"
 
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
